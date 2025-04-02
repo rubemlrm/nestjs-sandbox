0020b8f601c66bdb124e7e6612d69801a83b0ebc
Rubem Mota <rubemmota89@gmail.com>
2025-04-02 11:06:42 +0100

    refactor(ddd): testing ddd approach with nestjs

--- src/modules/recipe/ports/recipe.controller.ts
+++ src/modules/recipe/ports/recipe.controller.ts
@@ -11,36 +11,27 @@ import { CreateRecipeDto } from '../domain/recipe/create-recipe.dto';
 import { UpdateRecipeDto } from '../domain/recipe/update-recipe.dto';
 
 @Controller('recipe')
-//@UseInterceptors(CacheInterceptor)
 export class RecipeController {
-  //constructor(private readonly recipeService: RecipeService) { }
-
   @Post()
   create(@Body() createRecipeDto: CreateRecipeDto) {
     console.log('createRecipeDto', createRecipeDto);
-    //return this.recipeService.create(createRecipeDto);
   }
 
   @Get()
-  async findAll() {
-    //return await this.recipeService.findAll();
-  }
+  async findAll() {}
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
     console.log('findOne', id);
-    //return await this.recipeService.findOne(+id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
     console.log('updateRecipeDto', updateRecipeDto);
-    //return this.recipeService.update(+id, updateRecipeDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
     console.log('remove', id);
-    //return this.recipeService.remove(+id);
   }
 }
