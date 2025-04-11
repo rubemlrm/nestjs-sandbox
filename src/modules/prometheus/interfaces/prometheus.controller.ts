c50f9bbd714d3b2b56cfe9e62d92535011a79f37
rubemlrm <rubemmota89@gmail.com>
2025-04-11 10:59:25 +0100

    feat(metrics): add prisma metrics into app metrics (#8)

--- src/modules/prometheus/interfaces/prometheus.controller.ts
+++ src/modules/prometheus/interfaces/prometheus.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { PrometheusService } from '../app/service/prometheus.service';
-
+import { PrismaService } from '@src/modules/prisma/app/service/prisma.service';
 @Controller('metrics')
 export class PrometheusController {
-  constructor(private readonly prometheusService: PrometheusService) {}
+  constructor(
+    private readonly prometheusService: PrometheusService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   async getMetrics(@Res() res: Response) {
-    const metrics = await this.prometheusService.getMetrics();
+    const appMetrics = await this.prometheusService.getMetrics();
+    const prismaMetrics = await this.prismaService.$metrics.prometheus();
     res.setHeader('Content-Type', 'text/plain');
-    res.send(metrics);
+    res.send(appMetrics + prismaMetrics);
   }
 }
--- src/modules/prometheus/prometheus.module.ts
+++ src/modules/prometheus/prometheus.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrometheusService } from '@src/modules/prometheus/app/service/prometheus.service';
 import { PrometheusController } from '@src/modules/prometheus/interfaces/prometheus.controller';
-
+import { PrismaService } from '@src/modules/prisma/app/service/prisma.service';
 @Module({
-  providers: [PrometheusService],
+  providers: [PrometheusService, PrismaService],
   controllers: [PrometheusController],
 })
 export class PrometheusModule {}
